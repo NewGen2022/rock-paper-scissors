78a7480227e587c43d8d9e8e77a758cbf5e0637b
NewGen2022 <amirgimari@gmail.com>
2024-02-24 13:40:19 +0200

    Added new logic, structure of the page and styling

--- script.js
+++ script.js
@@ -1,43 +1,118 @@
-function getComputerChoice(){
-    choice = ['rock', 'paper', 'scissors'];
-    const random_index = Math.floor(Math.random() * 3);
+const buttons = document.querySelectorAll('button');
+const green = 'rgb(0, 255, 0)';
+const yellow = 'rgb(255, 255, 0)';
+const red = 'rgb(255, 0, 0)';
 
-    return choice[random_index];
-}
+const emojiMap = {
+    '🗿': 'rock',
+    '📃': 'paper',
+    '✂️': 'scissors'
+};
 
 let playerScore = 0;
 let computerScore = 0;
 
-function playRound(playerSelection, computerSelection){
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    return choices[Math.floor(Math.random() * 3)];
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = emojiMap[playerSelection];
+
     if (playerSelection === computerSelection) {
-        return "Tie";
+        return {
+            color: yellow,
+            phrase: 'Tie!'
+        };
     } else if (
         (playerSelection === 'paper' && computerSelection === 'rock') ||
         (playerSelection === 'rock' && computerSelection === 'scissors') ||
         (playerSelection === 'scissors' && computerSelection === 'paper')
     ) {
         playerScore += 1;
-        return "You Win a Round!";
+        return {
+            color: green,
+            phrase: 'You Win a Round!'
+        };
     } else {
         computerScore += 1;
-        return "You Lose a Round!";
-    }    
-} 
-
-function playGame(){
-    for (let i = 0; i < 5; i++){
-        const playerSelection = prompt("Rock, Paper or Scissors? Enter something: ").toLowerCase();
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-    }
-
-    if (playerScore > computerScore){
-        console.log("You Win a Match");
-    } else if(playerScore < computerScore){
-        console.log("You Lose a Match");
-    } else{
-        console.log("Tie on a Match");
+        return {
+            color: red,
+            phrase: 'You Lose a Round!'
+        };
     }
 }
 
-playGame()
+function updateDisplay(playerSelection, computerSelection, colorPhraseObj) {
+    const playerChoiceText = document.getElementById('playerChoice');
+    const computerChoiceText = document.getElementById('computerChoice');
+    const resultOfRound = document.getElementById('resultOfRound');
+    const playerScoreText = document.getElementById('playerScore');
+    const computerScoreText = document.getElementById('computerScore');
+
+    resultOfRound.style.color = colorPhraseObj.color;
+    resultOfRound.style.fontWeight = '900';
+    resultOfRound.textContent = colorPhraseObj.phrase;
+
+    playerChoiceText.textContent = `Your Choice: ${playerSelection}`;
+    computerChoiceText.textContent = `Computer Choice: ${computerSelection}`;
+
+    playerScoreText.textContent = `Score: ${playerScore}`;
+    computerScoreText.textContent = `Score: ${computerScore}`;
+}
+
+function endGameDisplay() {
+    const gameSection = document.getElementById('gameSection');
+    gameSection.style.display = 'none';
+
+    const endGameSection = document.getElementById('endGameSection');
+    endGameSection.style.display = 'block';
+
+    const gameResult = document.getElementById('gameResult');
+    const color = playerScore > computerScore ? green : red;
+
+    gameResult.textContent = playerScore > computerScore ?
+        `You Win ${playerScore}:${computerScore}` :
+        `Computer Wins ${computerScore}:${playerScore}`;
+
+    gameResult.style.color = color;
+
+    return [0, 0];
+}
+
+function playAgain() {
+    const resultOfRound = document.getElementById('resultOfRound');
+    const gameResult = document.getElementById('gameResult');
+    const gameSection = document.getElementById('gameSection');
+    const endGameSection = document.getElementById('endGameSection');
+
+    resultOfRound.textContent = '-';
+    resultOfRound.style.color = 'white';
+
+    gameResult.textContent = '';
+
+    const elementsToReset = ['playerChoice', 'computerChoice', 'playerScore', 'computerScore'];
+    elementsToReset.forEach(id => document.getElementById(id).textContent = id.includes('Score') ? 'Score: 0' : 'Your Choice: ');
+
+    gameSection.style.display = 'block';
+    endGameSection.style.display = 'none';
+}
+
+// Listen for click of the PlayerButton
+buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const playerSelection = btn.textContent.toLowerCase();
+        const computerSelection = getComputerChoice();
+        const colorPhraseObj = playRound(playerSelection, computerSelection);
+
+        updateDisplay(playerSelection, computerSelection, colorPhraseObj);
+
+        if (playerScore === 5 || computerScore === 5) {
+            [playerScore, computerScore] = endGameDisplay();
+
+            const playAgainButton = document.getElementById('playAgainButton');
+            playAgainButton.addEventListener('click', playAgain);
+        }
+    });
+});
