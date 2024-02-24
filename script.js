1b9b3b3d2e41bebf747cdb55e08a56f2ca8dbc60
NewGen2022 <amirgimari@gmail.com>
2024-02-24 22:16:34 +0200

    Added final stylesand logic

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ const emojiMap = {
     '✂️': 'scissors'
 };
 
+emojiPlayer = '👨‍💻';
+emojiComputer = '👾';
+
 let playerScore = 0;
 let computerScore = 0;
 
@@ -46,7 +49,6 @@ function playRound(playerSelection, computerSelection) {
 
 function updateDisplay(playerSelection, computerSelection, colorPhraseObj) {
     const playerChoiceText = document.getElementById('playerChoice');
-    const computerChoiceText = document.getElementById('computerChoice');
     const resultOfRound = document.getElementById('resultOfRound');
     const playerScoreText = document.getElementById('playerScore');
     const computerScoreText = document.getElementById('computerScore');
@@ -55,8 +57,18 @@ function updateDisplay(playerSelection, computerSelection, colorPhraseObj) {
     resultOfRound.style.fontWeight = '900';
     resultOfRound.textContent = colorPhraseObj.phrase;
 
+    if (colorPhraseObj.color === yellow) {
+        resultOfRound.style.textShadow = '0 0 7px yellow, 0 0 10px yellow, 0 0 21px yellow, 0 0 42px yellow';
+        resultOfRound.style.transition = 'text-shadow 0.3s linear'
+    } else if (colorPhraseObj.color === green) {
+        resultOfRound.style.textShadow = '0 0 7px green, 0 0 10px green, 0 0 21px green, 0 0 42px green';
+        resultOfRound.style.transition = 'text-shadow 0.3s linear'
+    } else if (colorPhraseObj.color === red) {
+        resultOfRound.style.textShadow = '0 0 7px red, 0 0 10px red, 0 0 21px red, 0 0 42px red';
+        resultOfRound.style.transition = 'text-shadow 0.3s linear'
+    }
+
     playerChoiceText.textContent = `Your Choice: ${playerSelection}`;
-    computerChoiceText.textContent = `Computer Choice: ${computerSelection}`;
 
     playerScoreText.textContent = `Score: ${playerScore}`;
     computerScoreText.textContent = `Score: ${computerScore}`;
@@ -72,9 +84,15 @@ function endGameDisplay() {
     const gameResult = document.getElementById('gameResult');
     const color = playerScore > computerScore ? green : red;
 
-    gameResult.textContent = playerScore > computerScore ?
-        `You Win ${playerScore}:${computerScore}` :
-        `Computer Wins ${computerScore}:${playerScore}`;
+    const emojiOfWinner = document.getElementById('emoji-size')
+    
+    if (playerScore > computerScore) {
+        emojiOfWinner.textContent = emojiPlayer;
+        gameResult.textContent = `You Win ${playerScore}:${computerScore}`;
+    } else {
+        emojiOfWinner.textContent = emojiComputer;
+        gameResult.textContent = `Computer Wins ${computerScore}:${playerScore}`;
+    }
 
     gameResult.style.color = color;
 
@@ -89,10 +107,20 @@ function playAgain() {
 
     resultOfRound.textContent = '-';
     resultOfRound.style.color = 'white';
+    resultOfRound.style.textShadow = 'none'
+
+    const computerButtonContainer = document.getElementById('computer');
+    const computerButtons = computerButtonContainer.querySelectorAll('button');
+    computerButtons.forEach(computerBtn => {
+        computerBtn.style.backgroundColor = '#fff'
+        computerBtn.style.boxShadow = 'none'
+        computerBtn.style.textShadow = 'none'
+        computerBtn.style.transition = 'none'
+    });
 
     gameResult.textContent = '';
 
-    const elementsToReset = ['playerChoice', 'computerChoice', 'playerScore', 'computerScore'];
+    const elementsToReset = ['playerChoice', 'playerScore', 'computerScore'];
     elementsToReset.forEach(id => document.getElementById(id).textContent = id.includes('Score') ? 'Score: 0' : 'Your Choice: ');
 
     gameSection.style.display = 'block';
@@ -107,6 +135,22 @@ buttons.forEach(btn => {
         const colorPhraseObj = playRound(playerSelection, computerSelection);
 
         updateDisplay(playerSelection, computerSelection, colorPhraseObj);
+
+        // Reset background color of computer buttons within the #computer div
+        const computerButtonContainer = document.getElementById('computer');
+        const computerButtons = computerButtonContainer.querySelectorAll('button');
+        computerButtons.forEach(computerBtn => {
+            computerBtn.style.backgroundColor = '#fff'
+            computerBtn.style.boxShadow = 'none'
+            computerBtn.style.textShadow = 'none'
+        });
+
+        // Change background color of the current computer selected button
+        const computerSelectedButton = document.getElementById(`${computerSelection}Computer`);
+        computerSelectedButton.style.backgroundColor = '#8000ff';
+        computerSelectedButton.style.boxShadow = '0 0 20px #8000ff'
+        computerSelectedButton.style.textShadow = '0 0 7px #8000ff, 0 0 10px #8000ff,0 0 21px #8000ff,0 0 42px #8000ff,0 0 82px #8000ff,0 0 92px #8000ff,0 0 102px #8000ff,0 0 151px #8000ff;'
+        computerSelectedButton.style.transition = 'background-color 0.3s linear, box-shadow 0.3s linear, text-shadow 0.3s linear'
 
         if (playerScore === 5 || computerScore === 5) {
             [playerScore, computerScore] = endGameDisplay();
